543a4af1d581036e028758b860614b91eb7b5345
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-07 08:59:29 +0000

    12

--- db/migrate/20231206092933_create_post_images.rb
+++ db/migrate/20231206092933_create_post_images.rb
@@ -1,9 +1,10 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-
-      t.string :table
-      t.text :body
+      
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
       t.timestamps
     end
   end
